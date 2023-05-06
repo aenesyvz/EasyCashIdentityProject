8c19f230a356259c44f9a700649171c1dfcd70d4
aenesyvz <20060403@stu.omu.edu.tr>
2023-05-06 14:14:46 +0300

    Add project files.

---
+++ Business/ValidationRules/AppUserValidationRules/CreateAppUserValidator.cs
@@ -0,0 +1,38 @@
+﻿using DtoLayer.Dtos.AppUserDtos;
+using EntityLayer.Concrete;
+using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLayer.ValidationRules.AppUserValidationRules
+{
+    public class CreateAppUserValidator : AbstractValidator<CreateAppUserDto>
+    {
+        public CreateAppUserValidator()
+        {
+            RuleFor(c => c.FirstName).NotEmpty().WithMessage("İsim alanı boş geçilemez");
+            RuleFor(c => c.LastName).NotEmpty().WithMessage("Soyad alanı boş geçilemez");
+
+            RuleFor(c => c.Email)
+                .NotEmpty().WithMessage("Email boş geçilemez")
+                .EmailAddress().WithMessage("Lütfen geçerli bir email adresi giriniz");
+
+            RuleFor(request => request.Password)
+                .NotEmpty().WithMessage("Şifre alanı boş geçilemez")
+                .MinimumLength(6).WithMessage("Şifre en az 6 karakterli olmalıdır")
+                .Matches("[A-Z]").WithMessage("Şifre en az 1 tane büyük harf içermelidir")
+                .Matches("[a-z]").WithMessage("Şifre en az 1 tane küçük harf içermelidir")
+                .Matches(@"\d").WithMessage("Şifre en az 1 tane rakam içermelidir")
+                .Matches(@"[][""!@$%^&*(){}:;<>,.?/+_=|'~\\-]").WithMessage("Şifre en az 1 tane özel karakter içermelidir");
+
+            RuleFor(c => c.ConfirmPassword)
+                .NotEmpty().WithMessage("Şifre tekrarı boş geçilemez")
+                 .Equal(x => x.Password).WithMessage("Şifreler uyuşmuyor");
+
+
+        }
+    }
+}
---
+++ Entities/Concrete/CustomerAccount.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EntityLayer.Concrete
+{
+    public class CustomerAccount
+    {
+        public int Id { get; set; }
+        public int AppUserId { get; set; }
+        public string AccountNumber { get; set; }
+        public string Currency { get; set; }
+        public decimal AccountBalance { get; set; }
+        public string BankBranch { get; set; }
+
+        public AppUser AppUser { get; set; }
+    }
+
+    public class CustomerAccountProcess
+    {
+        public int Id { get; set; }
+        public int ProcessType { get; set; }
+        public decimal Amount { get; set; }
+        public DateTime ProcessDate { get; set; }
+    }
+    public class AppUser:IdentityUser<int>
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string City { get; set; }
+        public string District { get; set; }
+        public string ImageUrl { get; set; }
+
+        public List<CustomerAccount> CustomerAccounts { get; set; }
+    }
+    public class AppRole : IdentityRole<int>
+    {
+
+    }
+}
